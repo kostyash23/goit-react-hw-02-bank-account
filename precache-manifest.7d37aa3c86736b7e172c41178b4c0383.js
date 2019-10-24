efebe603289d3563b3aee8fb94fe9a22825a3dc5
Kotya Shmotoloha <kostiashmotoloha@gmaol.com>
2019-10-24 12:09:34 +0300

    Updates

---
+++ precache-manifest.7d37aa3c86736b7e172c41178b4c0383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48a42aa442862d1cfa0d585ac15b371",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "38e5ebcdda9474109d36",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "504a7cb7ed47c2fa4bf5",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.e7bbb60d.chunk.css"
+  },
+  {
+    "revision": "38e5ebcdda9474109d36",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.4e963ca0.chunk.js"
+  },
+  {
+    "revision": "504a7cb7ed47c2fa4bf5",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.58b6489e.chunk.js"
+  },
+  {
+    "revision": "ca8b3cee66bec18bbf33",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.1accab51.js"
+  }
+]);
